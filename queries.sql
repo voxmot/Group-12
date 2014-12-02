4185956d48aac8f4a52741a84705c2aafc9b246a
James Choi <jchoi2012@knights.ucf.edu>
2014-12-01 22:11:22 -0500

    clean it

--- queries.sql
+++ queries.sql
@@ -1,9 +1,15 @@
 use dbUCF ; 
 
+
+/*-----------------------------------------------*/
+
 # Resources used by at least one student
 SELECT resource_name 
   FROM usage_record NATURAL JOIN resource
   GROUP BY resource_name;
+
+
+/*-----------------------------------------------*/
 
 
 # Resources used more than once
@@ -13,6 +19,7 @@ SELECT resource_name
   HAVING COUNT(resource_name) > 1;
 
 
+/*-----------------------------------------------*/
 
 
 # Last names of students who used desks
@@ -25,6 +32,8 @@ SELECT last_name
   GROUP BY last_name;  
 
 
+/*-----------------------------------------------*/
+
 
 # Resources a student with last name 'Carr' has used
 SELECT resource_name 
@@ -36,6 +45,8 @@ SELECT resource_name
   GROUP BY resource_name;
   
 
+/*-----------------------------------------------*/
+
 
 # Usage Records entries created between the days specified  
 SELECT * 
@@ -44,29 +55,17 @@ SELECT *
     AND usage_date < '2014-12-11';
   
 
-
-/*
-# Lists the last names of students who used chairs on December 1st  
-
-SELECT last_name 
-  FROM student NATURAL JOIN usage_record 
-  WHERE usage_date >'2014-12-1'
-    AND usage_date < '2014-12-2' 
-    AND resource_id IN(SELECT resource_id
-          FROM resource
-          WHERE resource_name = 'Chair');
-*/
-
-
-
+/*-----------------------------------------------*/
 
 
 # Last names of students who used chairs on December 1st  
-SELECT * FROM Resource NATURAL JOIN 
+SELECT last_name FROM Resource NATURAL JOIN 
        (SELECT *  FROM student NATURAL JOIN usage_record    
         WHERE usage_date >'2014-12-1' AND usage_date < '2014-12-2'  ) as OnDate 
 WHERE Resource.resource_name='Chairs' ;
 
+
+/*-----------------------------------------------*/
 
 
 # First Names of Students who have used the most resources with number of times used
@@ -78,7 +77,10 @@ SELECT first_name, withRecord.occurrances
   LIMIT 5;
 
 
-# First Names of students who have used the resources between the dates 2014-12-10 and 2014-12-11
+/*-----------------------------------------------*/
+
+
+# First Names of top 5 students who have used the MOST resources between the dates 2014-12-10 and 2014-12-11
 SELECT first_name, withRecord.occurrances 
   FROM Student NATURAL JOIN (select student_id, count(student_id) AS occurrances 
                                 FROM usage_record 
