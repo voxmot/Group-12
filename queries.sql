4ac9aa9754b6ba77474790f048073575a99a8cf7
voxmot <voxmot@gmail.com>
2014-12-01 19:37:06 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -24,3 +24,18 @@ SELECT resource_name
                         FROM student 
                         WHERE last_name = 'Carr')
   GROUP BY resource_name;
+  
+# Lists only usage record entries between the days specified  
+SELECT * 
+  FROM usage_record 
+  WHERE usage_date >'2014-12-10' 
+    AND usage_date < '2014-12-11';
+  
+# Lists the last names of students who used chairs on December 1st  
+SELECT last_name 
+  FROM student NATURAL JOIN usage_record 
+  WHERE usage_date >'2014-12-1'
+    AND usage_date < '2014-12-2' 
+    AND (SELECT resource_id
+          FROM resource
+          WHERE resource_name = 'Chair');
